b4e132ab970a3a2a3e74221a2574ec0e8cb6e295
Thiago Leandro dos santos <103519920+InfoSistemasPy@users.noreply.github.com>
2022-10-05 21:40:01 -0300

    ChatBot Inteligente

---
+++ Chatbot Youtube.py
@@ -0,0 +1,62 @@
+﻿#!/usr/bin/env python
+# coding: utf-8
+
+# ### Criando um chatbot Inteligente
+
+# In[6]:
+
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+# isso aqui só precisa para corrigir o bug
+from spacy.cli import download
+
+download("en_core_web_sm")
+
+class ENGSM:
+    ISO_639_1 = 'en_core_web_sm'
+
+
+# In[18]:
+
+
+chatbot = ChatBot("BOT_Thiago", tagger_language=ENGSM)
+
+conversa = [
+    "Eai",
+    "E aí, tranquilo?",
+    "Tranquilo",
+    "Qual a boa de hoje?",
+    "Eu estou ensinando Python e até chatbot",
+    "Caraca que doidera",
+    "Maneiro não é",
+    "Irado",
+]
+
+trainer = ListTrainer(chatbot)
+trainer.train(conversa)
+
+
+# In[21]:
+
+
+while True:
+    mensagem = input("Mande uma mensagem para o chatbot:")
+    if mensagem == "parar":
+        break
+    resposta = chatbot.get_response(mensagem)
+    print(resposta)
+
+
+# In[1]:
+
+
+chatbot.storage.drop()
+
+
+# In[ ]:
+
+
+
+
